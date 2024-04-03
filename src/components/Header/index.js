98c2e6da18c8b1b34ec206c3e6c436db41d18122
leandroleekrap <leandroleekrap@gmail.com>
2024-04-03 11:33:03 -0300

    Criando layou no header statusBar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,48 @@
 import React from "react";
-import { View, StyleSheet, Text, StatusBar } from "react-native";
+import { View, StyleSheet, Text, StatusBar, TouchableOpacity} from "react-native";
+import {Feather} from '@expo/vector-icons';
 
-const statusbarHeigth = StatusBar.currentHeight ? StatusBar.currentHeight  + 22 : 64;
+const statusbarHeigth = StatusBar.currentHeight ? StatusBar.currentHeight  + 22 : 60;
 
 export default function Header() {
   return (
     <View style={styles.container}>
-      <Text>Header do app</Text>
+        <View style={styles.content}>
+            <Text style= {styles.username}>Header do app</Text>
+            <TouchableOpacity activeOpacity={0.9} style={styles.buttonUser}>
+                <Feather name="user" size={27} color="#000"/>
+            </TouchableOpacity>    
+        </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#49f064',
+        //linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(73,240,100,1) 76%, rgba(0,212,255,1) 100%);
+        backgroundColor: 'rgb(131,58,180)',
         paddingTop: statusbarHeigth,
+        flexDirection: 'row',
+        paddingStart: 16,
+        paddingEnd: 16,
+        paddingBottom: 10,
+        borderBottomEndRadius: 20,
+        borderBottomStartRadius: 20,
+        },
+        content: {
+            flex: 1,
+            alignItems: 'center',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+        },
+        username: {
+            fontSize: 16,
+            color: '#000',
+            fontWeight: 'bold',
+        },
+        buttonUser: {
+            padding: 5,
+            borderRadius: 50,
+            backgroundColor: 'rgba(253,29,29,0.12368697478991597)',
         },
 });
